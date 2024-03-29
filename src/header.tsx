2f3714a4c61dff4a756b2c5f469c865848e68422
Misho <chapidze.misho95@gmail.com>
2024-03-29 19:16:28 +0400

    fxd

---
+++ src/header.tsx
@@ -0,0 +1,22 @@
+import { RiSearch2Line } from "react-icons/ri";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full bg-transparent">
+      <div>
+        <button className="size-[24px]">
+          <RiSearch2Line className="size-[16px]" />
+        </button>
+        <div>
+          <button className="size-[24px]">
+            <RxHamburgerMenu className="size-[16px]" />
+          </button>
+          <span className="uppercase font-inherit">menu</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
