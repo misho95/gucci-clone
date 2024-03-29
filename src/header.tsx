d4be952162e9bedc45fc0781e7f9447f53b8aab7
Misho <chapidze.misho95@gmail.com>
2024-03-29 19:54:36 +0400

    fxd

--- src/header.tsx
+++ src/header.tsx
@@ -1,18 +1,44 @@
+import { useWindowScroll } from "@uidotdev/usehooks";
+import clsx from "clsx";
 import { RiSearch2Line } from "react-icons/ri";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { GoPlus } from "react-icons/go";
 
 const Header = () => {
+  const [{ x, y }, scrollTo] = useWindowScroll();
+
   return (
-    <header className="fixed top-0 w-full bg-transparent font-gucci">
-      <div>
-        <button className="size-[24px]">
-          <RiSearch2Line className="size-[16px]" />
+    <header
+      className={clsx(
+        "fixed top-0 w-full bg-transparent font-sans px-20 duration-300",
+        {
+          "text-white bg-transparent": y === 0,
+          "text-black bg-white": y && y > 0,
+        }
+      )}
+    >
+      <div className="py-[1.5rem] px-[2rem] flex justify-between">
+        <button className="flex items-center gap-2 group">
+          <GoPlus className="group-hover:rotate-90" />
+          <span>Contact Us</span>
         </button>
-        <div>
+        <div className="flex gap-5 items-center ">
           <button className="size-[24px]">
-            <RxHamburgerMenu className="size-[16px]" />
+            <RiSearch2Line className="size-[16px]" />
           </button>
-          <span className="uppercase font-inherit">menu</span>
+
+          <button className="flex items-center h-[24px] gap-2">
+            <RxHamburgerMenu className="size-[16px]" />
+            <span className="uppercase font-inherit">menu</span>
+          </button>
+        </div>
+        <div
+          className={clsx("absolute left-1/2 -translate-x-1/2 duration-500", {
+            "text-[300px] top-0": y === 0,
+            "text-[40px] top-1": y && y > 0,
+          })}
+        >
+          GUCCI
         </div>
       </div>
     </header>
