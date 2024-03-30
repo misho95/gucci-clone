6186a027802d717e51e5b82e0ed9e57c100f6117
Misho <chapidze.misho95@gmail.com>
2024-03-30 18:05:27 +0400

    fxd

--- src/components/title-tag.tsx
+++ src/components/title-tag.tsx
@@ -4,7 +4,7 @@ type PropsType = {
 
 const TitleTag = ({ title }: PropsType) => {
   return (
-    <h2 className="backdrop-blur-[0.25rem] bg-white/10 text-white text-[0.75rem] font-bold py-[4pt] px-[8pt] rounded-[2pt] uppercase absolute top-[5rem] left-1/2 -translate-x-1/2 select-none">
+    <h2 className="backdrop-blur-[0.25rem] bg-white/10 text-white text-[0.75rem] font-bold py-[4pt] px-[8pt] rounded-[2pt] uppercase absolute top-[5rem] left-1/2 -translate-x-1/2 select-none z-20">
       {title}
     </h2>
   );
