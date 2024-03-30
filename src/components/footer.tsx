159262e470588bb97a22025a96e621c0ee6375be
Misho <chapidze.misho95@gmail.com>
2024-03-31 01:06:45 +0400

    fxdd

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import FooterLink from "./footer.link";
 const Footer = () => {
   return (
     <footer className="bg-black text-white px-[4rem] py-[5rem] w-full">
-      <div className="max-w-[1440px] m-auto grid grid-cols-4 gap-[50px_0]">
+      <div className="max-w-[1440px] m-auto grid grid-cols-1 sm:grid-cols-4 gap-[50px_0]">
         <div className="flex flex-col gap-2">
           <h3 className="font-bold text-[0.75rem] leading-[1rem] text-white/50 uppercase mb-[1.5rem]">
             MAY WE HELP YOU?
@@ -25,7 +25,7 @@ const Footer = () => {
           <FooterLink>Privacy & Cookie Policy</FooterLink>
           <FooterLink>Corporate Information</FooterLink>
         </div>
-        <div className="row-span-2 col-span-2 flex flex-col gap-2">
+        <div className="sm:row-span-2 sm:col-span-2 flex flex-col gap-2">
           <h3 className="font-bold text-[0.75rem] leading-[1rem] text-white/50 uppercase mb-[1.5rem]">
             STORE LOCATOR
           </h3>
@@ -47,7 +47,7 @@ const Footer = () => {
           <FooterLink>Discover our services</FooterLink>
           <FooterLink>Book an Appointment</FooterLink>
         </div>
-        <div className="col-span-4">
+        <div className="sm:col-span-4">
           © 2016 - 2022 Guccio Gucci S.p.A. - All rights reserved. SIAE LICENCE
           # 2294/I/1936 and 5647/I/1936
         </div>
