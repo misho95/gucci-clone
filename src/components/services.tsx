e4fce065ba582ca736d4b21ce1e34d5eebc72b2e
Misho <chapidze.misho95@gmail.com>
2024-03-30 19:33:43 +0400

    fxd

---
+++ src/components/services.tsx
@@ -0,0 +1,17 @@
+const Services = () => {
+  return (
+    <section>
+      <h1>GUCCI SERVICES</h1>
+      {/* image */}
+      <h4>BOOK AN APPOINTMENT</h4>
+      <p>
+        Enjoy priority access to the boutique of your choice at the time and
+        date that suits you. When you arrive, your Client Advisor will guide you
+        through a hand-picked selection of pieces for you to try-on and style.
+      </p>
+      <a>Book an In Store Appointment</a>
+    </section>
+  );
+};
+
+export default Services;
