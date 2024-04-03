1a19dc0e2dcd0129261aa40acbb4bcbab04131e5
Misho <chapidze.misho95@gmail.com>
2024-04-03 13:25:17 +0400

    fxd

--- src/components/featured.tsx
+++ src/components/featured.tsx
@@ -1,15 +1,48 @@
+import { useEffect, useState } from "react";
 import Image from "../assets/HP_Portrait_Gucci-G-Timeless-Feb24-SHOT_05_HS_231219_GUCCI_LOAFERS_0451_001_Default.avif";
 import SpinButton from "./spin.button";
+import { useIntersectionObserver } from "@uidotdev/usehooks";
+import { animated, useSpring } from "@react-spring/web";
 
 const Featured = () => {
+  const [stop, setStop] = useState(false);
+  const [ref, entry] = useIntersectionObserver({
+    threshold: 0,
+    root: null,
+    rootMargin: "0px",
+  });
+
+  const [spring, animate] = useSpring(() => ({
+    from: { transform: "scale(120%)" },
+  }));
+
+  useEffect(() => {
+    if (entry?.isIntersecting && !stop) {
+      animate.start({
+        from: { transform: "scale(120%)" },
+        to: { transform: "scale(100%)" },
+        config: {
+          duration: 800,
+        },
+        delay: 500,
+      });
+
+      setStop(true);
+    }
+  }, [entry?.isIntersecting]);
+
   return (
     <section className="mt-[4rem] sm:mt-[8rem] mx-[1rem] sm:mx-[4rem] mb-[5rem] sm:mb-[10rem] max-w-[1440px] ">
       <h3 className="mb-[5rem] sm:mb-[10rem] text-[1rem] font-bold uppercase w-full text-center">
         featured
       </h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-[2rem]">
-        <figure className="w-full aspect-[0.75] sm:w-[calc(100%-8rem)] h-fit relative">
-          <img
+        <figure
+          ref={ref}
+          className="w-full aspect-[0.75] sm:w-[calc(100%-8rem)] h-fit relative overflow-hidden"
+        >
+          <animated.img
+            style={spring}
             src={Image}
             className="w-full h-full object-cover brightness-[0.90]"
           />
