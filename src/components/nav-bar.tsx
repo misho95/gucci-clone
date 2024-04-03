6fd89496518a758d9fab67f77e8f0d60078db32d
Misho <chapidze.misho95@gmail.com>
2024-04-03 13:22:22 +0400

    fxdd

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -23,7 +23,7 @@ const NavBar = ({
     <div
       onClick={() => set(false)}
       className={clsx(
-        "w-full h-full bg-black/50 backdrop-blur-md fixed top-0 left-0 z-50 duration-300 flex justify-end",
+        "w-full h-full bg-black/50 backdrop-blur-md fixed top-0 left-0 z-50 duration-300 flex justify-end overflow-y-auto",
         {
           "opacity-0 pointer-events-none": !open,
           "opacity-100": open,
@@ -33,41 +33,45 @@ const NavBar = ({
       <div
         onClick={(e) => e.stopPropagation()}
         className={clsx(
-          "h-full w-full sm:w-fit p-[5rem] bg-white duration-700 relative ease-[cubic-bezier(0.5,0,0,1)] overflow-y-auto overflow-x-hidden",
+          "h-fit w-full sm:w-fit bg-white duration-700 relative ease-[cubic-bezier(0.5,0,0,1)]",
           {
             "translate-y-full sm:translate-y-0 sm:translate-x-full": !open,
             "translate-y-0 sm:translate-x-0": open,
           }
         )}
       >
-        <button
-          onClick={() => set(false)}
-          className="bg-black rounded-full size-[48px] absolute top-1 right-1 flex justify-center items-center hover:scale-75 duration-500"
-        >
-          <IoMdClose className="size-[24px] text-white" />
-        </button>
-        <div className="w-full sm:w-[420px] h-full flex flex-col gap-[2.5rem] mb-[10rem]">
-          <nav className="flex flex-col gap-[1.5rem]">
-            <NavLinks title="new in" />
-            <NavLinks title="handbags" />
-            <NavLinks title="travel" />
-            <NavLinks title="women" />
-            <NavLinks title="men" />
-            <NavLinks title="children" />
-            <NavLinks title="jewerly & watches" />
-            <NavLinks title="decor & lifestyle" />
-            <NavLinks title="fragrances & make-up" />
-            <NavLinks title="gifts" />
-          </nav>
-          <div className="flex flex-col gap-[1rem] pb-[2rem]">
-            <NavLinks title="gucci services" style={{ fontSize: "1rem" }} />
-            <NavLinks title="world of gucci" style={{ fontSize: "1rem" }} />
-          </div>
-          <div className="flex flex-col gap-[2rem] pt-[3rem] px-[0.25rem]">
-            <AnimatedLinks title={"sign in"} color="black" />
-            <AnimatedLinks title={"my orders"} color="black" />
-            <AnimatedLinks title={"contact us"} color="black" />
-            <AnimatedLinks title={"+1 8774822430"} color="black" />
+        <div className="sticky top-1 right-1 flex justify-end p-1">
+          <button
+            onClick={() => set(false)}
+            className="bg-black rounded-full size-[48px] flex justify-center items-center hover:scale-75 duration-500 z-50"
+          >
+            <IoMdClose className="size-[24px] text-white" />
+          </button>
+        </div>
+        <div className="p-[1.5rem] sm:p-[5rem] w-full">
+          <div className="w-full sm:w-[420px] h-full flex flex-col gap-[2.5rem]">
+            <nav className="flex flex-col gap-[1.5rem]">
+              <NavLinks title="new in" />
+              <NavLinks title="handbags" />
+              <NavLinks title="travel" />
+              <NavLinks title="women" />
+              <NavLinks title="men" />
+              <NavLinks title="children" />
+              <NavLinks title="jewerly & watches" />
+              <NavLinks title="decor & lifestyle" />
+              <NavLinks title="fragrances & make-up" />
+              <NavLinks title="gifts" />
+            </nav>
+            <div className="flex flex-col gap-[1rem] pb-[2rem]">
+              <NavLinks title="gucci services" style={{ fontSize: "1rem" }} />
+              <NavLinks title="world of gucci" style={{ fontSize: "1rem" }} />
+            </div>
+            <div className="flex flex-col gap-[2rem] pt-[3rem] px-[0.25rem]">
+              <AnimatedLinks title={"sign in"} color="black" />
+              <AnimatedLinks title={"my orders"} color="black" />
+              <AnimatedLinks title={"contact us"} color="black" />
+              <AnimatedLinks title={"+1 8774822430"} color="black" />
+            </div>
           </div>
         </div>
       </div>
